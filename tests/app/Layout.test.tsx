e9a4b1dae2b63aecfadab969ed0e31068f46eb0d
Csaba Babos <babos.csaba@yourweb.hu>
2024-07-12 11:16:02 +0200

    More tests

--- tests/app/Layout.test.tsx
+++ tests/app/Layout.test.tsx
@@ -1,13 +1,13 @@
-import { InnerLayout, metadata } from "@/app/layout";
-import { render } from "@testing-library/react";
+import { InnerLayout, metadata } from '@/app/layout';
+import { render } from '@testing-library/react';
 
 describe('Root layout', () => {
-    it('should render the main layout with navigation', () => {
-        const { getByText } = render(InnerLayout(<h1>Hello world</h1>));
-        expect(getByText('Home')).toBeInTheDocument();
-    });
+  it('should render the main layout with navigation', () => {
+    const { getByText } = render(InnerLayout(<h1>Hello world</h1>));
+    expect(getByText('Home')).toBeInTheDocument();
+  });
 
-    it('should have proper metadata', () => {
-        expect(metadata.title).toStrictEqual("PressKit SM Home");
-    })
+  it('should have proper metadata', () => {
+    expect(metadata.title).toStrictEqual('PressKit SM Home');
+  });
 });
--- tests/app/entity/crud.test.ts
+++ tests/app/entity/crud.test.ts
@@ -2,21 +2,23 @@ import mockData from '@/../tests/mockEntities.json';
 import { getAllItems, getEntityBy } from '@/app/entity/crud';
 
 describe('crud', () => {
-    it('should return all available data', () => {
-        const result = getAllItems();
+  it('should return all available data', () => {
+    const result = getAllItems();
 
-        expect(result).toStrictEqual(mockData);
-    })
+    expect(result).toStrictEqual(mockData);
+  });
 
-    it('should return the second child of the root entity', () => {
-        const result = getEntityBy(3);
+  it('should return the second child of the root entity', () => {
+    const result = getEntityBy(3);
 
-        expect(result).toStrictEqual(mockData[0].children[1]);
-    });
+    expect(result).toStrictEqual(mockData[0].children[1]);
+  });
 
-    it('should throw an error when an item is not found', () => {
-        const caller = () => { return getEntityBy(666); };
+  it('should throw an error when an item is not found', () => {
+    const caller = () => {
+      return getEntityBy(666);
+    };
 
-        expect(caller).toThrow(Error);
-    })
+    expect(caller).toThrow(Error);
+  });
 });
