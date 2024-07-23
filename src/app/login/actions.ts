9fa3b2726d87772d8a3ddd61e7bcf17a15bddbb4
Csaba Babos <babos.csaba@yourweb.hu>
2024-07-23 14:58:56 +0200

    cleanup

--- src/app/login/actions.ts
+++
@@ -1,8 +0,0 @@
-// TODO: Create dynamic login information
-export async function handleLogin(
-  username: string,
-  password: string
-): Promise<boolean> {
-  if (username === 'cbabos' && password === 'x') return true;
-  return false;
-}
