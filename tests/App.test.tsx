ff66ff794429c847ccc35eb22b9753d05eb722b2
Csaba Babos <babos.csaba@yourweb.hu>
2024-07-30 10:52:35 +0200

    Added test coverage

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -2,9 +2,15 @@ import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
 import App from '../src/App';
 
-test('Should render the hello world app', () => {
+test('Should render the hello world app', async () => {
 	render(<App />);
 	
 	const title = screen.getByText('Vite + React');
 	expect(title.tagName).toBe('H1');
+	
+	const buttonToInc = screen.getByTestId('increase-btn');
+	expect(buttonToInc).toBeVisible();
+	buttonToInc.click();
+
+	await screen.findByText('count is 1');
 });
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	test: {
+		coverage: {
+			exclude: [
+				'src/main.tsx',
+				'.eslint*',
+				'vite.config*',
+				'**/*.d.ts',
+			],
+			thresholds: {
+				branches: 80,
+				lines: 80,
+				statements: 80,
+				functions: 80,
+			}
+		},
 		environment: 'jsdom',
 		setupFiles: [ 'vitest.setup.ts' ],
 	},
